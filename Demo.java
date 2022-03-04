0e75c913acb9a92caea9f8c53448b70c8d860e25
MuhammadQureshi <MuhammadAtif.Qureshi@uts.edu.au>
2022-03-04 15:26:51 +1100

    First commit after parallelism

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
+
+
 public class Demo{
     public static void main (String args[]){
         System.out.println("Welcome to PBT205");
+        System.out.println("A new line after parallelism was granted");
     }
 }
